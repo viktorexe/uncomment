64aeab50630e110f4391524400bf5eaaf89479c0
viktorexe <developer.viktorexe@gmail.com>
2025-07-28 21:24:37 +0530

    re-made again

--- backend/core/ruby_processor.py
+++
@@ -1,8 +0,0 @@
-from .base_processor import BaseProcessor
-from typing import List, Tuple
-
-class RubyProcessor(BaseProcessor):
-    def get_comment_patterns(self) -> Tuple[List[str], List[Tuple[str, str]]]:
-        single_patterns = ['#']
-        multi_patterns = [('=begin', '=end')]
-        return single_patterns, multi_patterns
